7d48e621ec3bbeee4681c7da4514c5dc523964e4
Bakels <ricobakels2@hotmail.com>
2017-11-07 22:33:32 +0100

    minor changes

--- src/main/java/com/presentie/administratie/registrationservice/controllers/StatusController.java
+++ src/main/java/com/presentie/administratie/registrationservice/controllers/StatusController.java
@@ -2,9 +2,14 @@ package com.presentie.administratie.registrationservice.controllers;
 
 
 import com.presentie.administratie.registrationservice.models.Status;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,6 +23,26 @@ public class StatusController {
     @RequestMapping(method = RequestMethod.GET)
     public List<Status> getStatusList(){
         return statusList;
+    }
+
+    @RequestMapping(path = "/add", method = RequestMethod.POST)
+    public void addStatusToList(@RequestBody Status status){
+        statusList.add(status);
+
+
+    }
+
+    @RequestMapping(path = "/test")
+    public void test(){
+        Status status = new Status("Rico", true);
+
+        RestTemplate build = new RestTemplateBuilder().build();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json");
+        HttpEntity<Status> entity = new HttpEntity<>(status, headers);
+
+        Status status1 = build.postForObject("http://localhost:8001/api/status/add", entity, Status.class);
     }
 
 }
--- src/main/java/com/presentie/administratie/registrationservice/receivers/StatusReceiver.java
+++ src/main/java/com/presentie/administratie/registrationservice/receivers/StatusReceiver.java
@@ -1,13 +1,27 @@
 package com.presentie.administratie.registrationservice.receivers;
 
+import com.presentie.administratie.registrationservice.controllers.StatusController;
+import com.presentie.administratie.registrationservice.models.Status;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.web.client.RestTemplate;
 
 @RabbitListener(queues = "status")
 public class StatusReceiver {
 
+    @Autowired
+    StatusController controller;
+
     @RabbitHandler
-    public void receive(String in){
-        System.out.printf(in);
+    public void receive(Status receivedStatus){
+        System.out.printf(String.valueOf(receivedStatus));
+
+        RestTemplateBuilder builder = new RestTemplateBuilder();
+        RestTemplate build = builder.build();
+
+        Status status = new Status("Rico", true);
+        build.postForObject("/api/status/add", receivedStatus, Status.class);
     }
 }
